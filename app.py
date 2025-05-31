4d07d090c996bceae03b20836c981bbbb86baa78
jvredmerski <jv.redmerski@gmail.com>
2025-05-30 23:38:25 -0300

    Interligando as páginas para poder navegar entre elas

--- app.py
+++ app.py
@@ -40,5 +40,9 @@ def pedidos():
     pedidos = cur.fetchall()
     return render_template('index.html', pedidos=pedidos)
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
